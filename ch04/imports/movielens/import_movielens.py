e2f2eb8107d7f23a021595c0eac413473045eae3
Alex Ott <alexott@gmail.com>
2020-05-24 17:29:40 +0200

    fix imports problems

--- ch04/imports/movielens/import_movielens.py
+++ ch04/imports/movielens/import_movielens.py
@@ -1,27 +1,32 @@
 import csv
 import time
-from neo4j.v1 import GraphDatabase
+from neo4j import GraphDatabase
 from imdb import IMDb
-
+import sys
 
 class MoviesImporter(object):
 
     def __init__(self, uri, user, password):
-        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+        self._driver = GraphDatabase.driver(uri, auth=(user, password), encrypted=0)
         self._ia = IMDb()
 
     def close(self):
         self._driver.close()
 
+    def executeNoException(self, session, query):
+        try:
+            session.run(query)
+        except Exception as e:
+            pass
+    
     def import_movies(self, file):
+        print("import movies")
         with open(file, 'r+') as in_file:
             reader = csv.reader(in_file, delimiter=',')
             next(reader, None)
             with self._driver.session() as session:
-                session.run(
-                    "CREATE CONSTRAINT ON (a:Movie) ASSERT a.movieId IS UNIQUE; ")
-                session.run(
-                    "CREATE CONSTRAINT ON (a:Genre) ASSERT a.genre IS UNIQUE; ")
+                self.executeNoException(session, "CREATE CONSTRAINT ON (a:Movie) ASSERT a.movieId IS UNIQUE; ")
+                self.executeNoException(session, "CREATE CONSTRAINT ON (a:Genre) ASSERT a.genre IS UNIQUE; ")
 
                 tx = session.begin_transaction()
 
@@ -34,11 +39,11 @@ class MoviesImporter(object):
                             title = strip(row[1])
                             genres = strip(row[2])
                             query = """
-                                MERGE (movie:Movie {movieId: {movieId}, title: {title}})
-                                with movie
-                                UNWIND {genres} as genre
-                                MERGE (g:Genre {genre: genre})
-                                MERGE (movie)-[:HAS_GENRE]->(g)
+                            CREATE (movie:Movie {movieId: $movieId, title: $title})
+                            with movie
+                            UNWIND $genres as genre
+                            MERGE (g:Genre {genre: genre})
+                            MERGE (movie)-[:HAS_GENRE]->(g)
                             """
                             tx.run(query, {"movieId": movie_id, "title": title, "genres": genres.split("|")})
                             i += 1
@@ -46,20 +51,23 @@ class MoviesImporter(object):
 
                         if i == 1000:
                             tx.commit()
-                            print(j, "lines processed")
+                            print(j, "movies processed")
                             i = 0
                             tx = session.begin_transaction()
+                            
                     except Exception as e:
                         print(e, row, reader.line_num)
+                        
                 tx.commit()
                 print(j, "lines processed")
 
     def import_movie_details(self, file):
+        print("Importing details of movies")
         with open(file, 'r+') as in_file:
             reader = csv.reader(in_file, delimiter=',')
             next(reader, None)
             with self._driver.session() as session:
-                session.run("CREATE CONSTRAINT ON (a:Person) ASSERT a.name IS UNIQUE;")
+                self.executeNoException(session, "CREATE CONSTRAINT ON (a:Person) ASSERT a.name IS UNIQUE;")
                 tx = session.begin_transaction()
                 i = 0;
                 j = 0;
@@ -75,23 +83,24 @@ class MoviesImporter(object):
 
                         if i == 10:
                             tx.commit()
-                            print(j, "lines processed")
+                            print(j, "movie details imported")
                             i = 0
                             tx = session.begin_transaction()
                     except Exception as e:
                         print(e, row, reader.line_num)
+                        
                 tx.commit()
                 print(j, "lines processed")
 
     def process_movie_info(self, tx, movie_info, movie_id):
         query = """
-            MATCH (movie:Movie {movieId: {movieId}})
-            SET movie.plot = {plot}
-            FOREACH (director IN {directors} | MERGE (d:Person {name: director}) SET d:Director MERGE (d)-[:DIRECTED]->(movie))
-            FOREACH (actor IN {actors} | MERGE (d:Person {name: actor}) SET d:Actor MERGE (d)-[:ACTS_IN]->(movie))
-            FOREACH (producer IN {producers} | MERGE (d:Person {name: producer}) SET d:Producer MERGE (d)-[:PRODUCES]->(movie))
-            FOREACH (writer IN {writers} | MERGE (d:Person {name: writer}) SET d:Writer MERGE (d)-[:WRITES]->(movie))
-            FOREACH (genre IN {genres} | MERGE (g:Genre {genre: genre}) MERGE (movie)-[:HAS_GENRE]->(g))
+        MATCH (movie:Movie {movieId: $movieId})
+        SET movie.plot = $plot
+        FOREACH (director IN $directors | MERGE (d:Person {name: director}) SET d:Director MERGE (d)-[:DIRECTED]->(movie))
+        FOREACH (actor IN $actors | MERGE (d:Person {name: actor}) SET d:Actor MERGE (d)-[:ACTS_IN]->(movie))
+        FOREACH (producer IN $producers | MERGE (d:Person {name: producer}) SET d:Producer MERGE (d)-[:PRODUCES]->(movie))
+        FOREACH (writer IN $writers | MERGE (d:Person {name: writer}) SET d:Writer MERGE (d)-[:WRITES]->(movie))
+        FOREACH (genre IN $genres | MERGE (g:Genre {genre: genre}) MERGE (movie)-[:HAS_GENRE]->(g))
         """
         directors = []
         for director in movie_info['directors']:
@@ -121,18 +130,17 @@ class MoviesImporter(object):
             plot = movie_info['plot outline']
 
         tx.run(query, {"movieId": movie_id, "directors": directors, "genres": genres, "actors": actors, "plot": plot,
-                       "writers": writers, "producers": producers})
+                "writers": writers, "producers": producers})
 
     def import_user_item(self, file):
         with open(file, 'r+') as in_file:
             reader = csv.reader(in_file, delimiter=',')
             next(reader, None)
             with self._driver.session() as session:
-                session.run("CREATE CONSTRAINT ON (u:User) ASSERT u.userId IS UNIQUE")
+                self.executeNoException(session, "CREATE CONSTRAINT ON (u:User) ASSERT u.userId IS UNIQUE")
 
                 tx = session.begin_transaction()
                 i = 0;
-                j = 0;
                 for row in reader:
                     try:
                         if row:
@@ -140,25 +148,20 @@ class MoviesImporter(object):
                             movie_id = strip(row[1])
                             rating = strip(row[2])
                             timestamp = strip(row[3])
-
                             query = """
-                                MATCH (movie:Movie {movieId: {movieId}})
-                                MERGE (user:User {userId: {userId}})
-                                MERGE (user)-[:RATES {rating: {rating}, timestamp: {timestamp}}]->(movie)
+                            MATCH (movie:Movie {movieId: $movieId})
+                            MERGE (user:User {userId: $userId})
+                            MERGE (user)-[:RATES {rating: $rating, timestamp: $timestamp}]->(movie)
                             """
                             tx.run(query, {"movieId":movie_id, "userId": user_id, "rating":rating, "timestamp": timestamp})
                             i += 1
-                            j += 1
-
-                        if i == 1000:
-                            tx.commit()
-                            print(j, "lines processed")
-                            i = 0
-                            tx = session.begin_transaction()
+                            if i == 1000: 
+                                tx.commit()
+                                i = 0
+                                tx = session.begin_transaction()
                     except Exception as e:
                         print(e, row, reader.line_num)
                 tx.commit()
-                print(j, "lines processed")
 
 def strip(string): return ''.join([c if 0 < ord(c) < 128 else ' ' for c in string])
 
@@ -166,8 +169,14 @@ def strip(string): return ''.join([c if 0 < ord(c) < 128 else ' ' for c in strin
 if __name__ == '__main__':
     start = time.time()
     uri = "bolt://localhost:7687"
-    importing = MoviesImporter(uri=uri, user="neo4j", password="pippo1")
-    importing.import_movies(file="/Users/ale/neo4j-servers/gpml/dataset/ml-latest-small/movies.csv")
-    importing.import_movie_details(file="/Users/ale/neo4j-servers/gpml/dataset/ml-latest-small/links.csv")
+    user = "neo4j"
+    password = "q1" # pippo1
+    importing = MoviesImporter(uri=uri, user=user, password=password)
+    base_path = "/Users/ale/neo4j-servers/gpml/dataset/ml-latest-small"
+    if (len(sys.argv) > 1):
+        base_path = sys.argv[1]
+    importing.import_movies(file=base_path + "/movies.csv")
+    importing.import_movie_details(file=base_path + "/links.csv")
+    importing.import_user_item(file=base_path + "/ratings.csv")
     end = time.time() - start
     print("Time to complete:", end)
